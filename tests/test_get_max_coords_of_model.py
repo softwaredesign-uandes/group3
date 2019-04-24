0eb9f39a66f9e2d1dae5933fae786da5c8cb7668
e440 <jvejar1@miuandes.cl>
2019-04-24 00:35:04 -0400

    Test added to check the maximum coordinates of a model

---
+++ tests/test_get_max_coords_of_model.py
@@ -0,0 +1,74 @@
+import unittest
+import sys
+import mock
+
+sys.path.append('.')
+from main import get_max_coords_of_model
+
+
+class GetMaxCoordsOfModelTestCase(unittest.TestCase):
+
+    def test_max_coords_of_model(self):
+        block_model = {
+            '0': {
+                '0': {
+                    '0': {
+                        'minerals': {
+                            'copper': '71550'
+                        },
+                        'weight': '71550.01563'
+                    }
+                }
+            },
+            '14': {
+                '17': {
+                    '19': {
+                        'minerals': {
+                            'copper': '71670'
+                        },
+                        'weight': '71670.01563'
+                    }
+                }
+            },
+            '15': {
+                '17': {
+                    '10': {
+                        'minerals': {
+                            'copper': '72240'
+                        },
+                        'weight': '72240.00781'
+                    }
+                }
+            },
+            '16': {
+                '18': {
+                    '10': {
+                        'minerals': {
+                            'copper': '72360'
+                        },
+                        'weight': '72360'
+                    }
+                }
+            },
+            '17': {
+                '17': {
+                    '10': {
+                        'minerals': {
+                            'copper': '72360'
+                        },
+                        'weight': '72360'
+                    }
+                }
+            }
+        }
+
+        x_result, y_result, z_result = 17,18,19
+        x_max, y_max, z_max = get_max_coords_of_model(block_model)
+        self.assertEqual(x_max,x_result)
+        self.assertEqual(y_max,y_result)
+        self.assertEqual(z_max,z_result)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
