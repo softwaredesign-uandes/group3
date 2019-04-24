4c5b170741a8b7959006c5d41ddaa45b2cf9941f
e440 <jvejar1@miuandes.cl>
2019-04-24 00:52:10 -0400

    fix test max coords of a void model

--- tests/test_get_max_coords_of_model.py
+++ tests/test_get_max_coords_of_model.py
@@ -11,7 +11,7 @@ class GetMaxCoordsOfModelTestCase(unittest.TestCase):
 
     def test_max_coords_of_a_void_model(self):
         block_model={}
-        x_result, y_result, z_result = None,None,None
+        x_result, y_result, z_result = -1,-1,-1
         x_max, y_max, z_max = get_max_coords_of_model(block_model)
         self.assertEqual(x_max, x_result)
         self.assertEqual(y_max, y_result)
